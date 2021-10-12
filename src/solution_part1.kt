15480e40276977a95b2e67031649e3b61f333010
RajwaAbdulaziz <Rajwa.Abdulaziz.jed.k2@tuwaiq.edu.sa>
2021-10-12 22:58:45 +0300

    Last challenge took me a while, but I did my best.

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,48 @@
-fun main(){   }
+fun main() {
+
+    //Q1
+    println("Please enter your choice between 1. Sum Numbers 2. Concatenate Full Name 0. to Exit The Loop")
+
+    while (true) {
+
+        val choice = Integer.valueOf(readLine())
+
+        //Q1
+        if (choice == 1) {
+
+            println("Please enter 2 numbers to get the sum")
+
+            println("The sum is: " + sum(Integer.valueOf(readLine()), Integer.valueOf(readLine())))
+
+            //Q2
+        } else if (choice == 2) {
+
+            println("Please enter your first, middle and last name to combine them")
+
+            println("The full name is: " + fullName(readLine(), readLine(), readLine()))
+
+        } else if (choice == 0) {
+
+            println("Goodbye.")
+            break
+
+        }
+        else
+            println("please choose either 1 or 2 or 0")
+
+    }
+}
+
+    //Q1
+fun sum(num1: Int, num2: Int): Int {
+
+    return num1 + num2
+
+}
+
+    //Q2
+fun fullName(firstName: String?, middleName: String?, lastName: String?): String {
+
+    return "$firstName $middleName $lastName"
+
+}
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,60 @@
+import kotlin.system.exitProcess
+
+var myFireballs = 50
+
 fun main(){
 
+    //Q3
+   println("The largest number is: " + maximus(5, 6, 7))
+
+    //Challenge 1
+
+   println(math(2, 3))
+
+    //Challenge 2
+
+    while (myFireballs > 1) {
+        println(castFireball(Integer.valueOf(readLine())))
+    }
+}
+
+    //Q3
+val maximus: (Int, Int, Int) -> Int = { num1, num2, num3 ->
+
+    maxOf(num1, num2, num3)
+
+}
+
+   //Challenge 1
+
+fun math(num1: Int, num2: Int): String {
+
+    val add = num1 + num2
+    val subtract = num1 - num2
+    val multiply = num1 * num2
+    val divide = num1 / num2
+
+    return " Add: $add \n Subtract: $subtract \n Multiply: $multiply \n Divide: $divide"
+}
+
+   //Challenge 2
+
+
+fun castFireball(useFireball: Int): Int{
+
+//    val maxFireball= 50
+//    val minFireballs = 1
+
+
+        if(useFireball <= myFireballs) {
+
+            myFireballs -= useFireball
+
+        } else {
+            println("The number of fireball you want to cast exceeds the amount you have.")
+            exitProcess(0)
+        }
+
+
+    return myFireballs
 }
