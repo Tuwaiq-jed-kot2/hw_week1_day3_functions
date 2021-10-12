334d9a1bbf176faaf603e10bfac691a9854e6c76
Qussai <qussai.saeed.jed.k2@tuwaiq.edu.sa>
2021-10-12 23:08:21 +0300

    Problem Solved :)

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,8 @@
-fun main(){   }
+fun main(){
+    fun add(num1:Int , num2:Int):Int{
+        return num1+num2
+    }
+    val result = add(2,7)
+
+println(result)
+}
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,8 @@
 fun main(){
+    fun fName(first:String,middle:String,last:String):String{
+        return "$first $middle $last"
 
+    }
+    val fullName=fName("Qussai","Saeed","Alotabi")
+    println(fullName)
 }
