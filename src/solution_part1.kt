445f359cc80574d80658962c1a367fcb15ba41a8
SomyahAA <somyah.abdulaziz.jed.k2@tuwaiq.edu.sa>
2021-10-13 02:14:29 +0300

    :)

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,14 @@
-fun main(){   }
+fun main(){
+
+     println(add(10,12))
+
+     println(fullName("Ahmed","Abdullah","Al Waleed"))
+
+}
+     fun add(num1:Int,num2:Int):Int{
+         return num1+num2
+     }
+
+     fun fullName(fName:String,mName:String,lName:String):String{
+         return "$fName $mName $lName"
+     }
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,46 @@
 fun main(){
 
+    println(math(2,3))
+    castFireball(5)
+    //println(maximum(10,14,12))
 }
+
+
+    fun math(num1 :Int, num2:Int){
+        println("Add : "+(num1+num2))
+        println("Subtract : "+(num1-num2))
+        println("Multiply : "+(num1*num2))
+        println("Divide : "+(num1/num2))
+    }
+
+    fun castFireball(numFireballs:Int){
+        val result = when (numFireballs){
+
+             1 ->10
+             2 ->20
+             3-> 30
+             4-> 40
+             5-> 50
+            else -> 0
+        }
+        println("result is $result")
+    }
+
+
+  // I tried to solve it, but I kinda missed up everything :(
+  /*val maximum: (num1:Int,num2:Int,num3:Int)-> Unit ={
+
+       if (num1>num2){
+          if (num1>num3){
+              println("The largest number is $num1")
+          }
+          else{
+              println("The largest number is $num3")
+          }
+       }else{
+           if (num2>num3){
+               println("The largest number is $num2")
+           } else
+               println("The largest number is $num3")
+       }
+  }*/
