2b23d9c206e0f2e191b62625c5aff1d7ff1e8350
bayanalharthy <Bayan.Mastour.jed.k2@tuwaiq.edu.sa>
2021-10-13 04:01:47 +0300

    Problem Solved :)

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,28 @@
-fun main(){   }
+
+fun main()
+//{
+//
+//    val N1 = 10
+//    val N2 = 12
+//
+//    println(add(N1,N2))
+//
+//}
+//fun add(N1:Int, N2:Int ) : Int {
+//
+//    return N1+N2
+//}
+
+{
+
+
+
+     println(fullName( "Bayan", "Mastour", "ALharthy"))
+    }
+    fun fullName(firstName:String ,midName:String, lastName:String):String{
+        return "$firstName $midName $lastName"
+    }
+
+
+
+
