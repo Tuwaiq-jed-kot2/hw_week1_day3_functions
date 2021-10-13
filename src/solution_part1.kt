557b10458717f6f7710807adc5e081b5645ce762
asus <Asma-Almahdawi>
2021-10-13 08:13:49 +0300

    that easy thank you

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,6 @@
-fun main(){   }
+fun main() {
+    println(add(10,12))
+}
+fun add(num1 : Int , num2 : Int ) : Int{
+    return num1+ num2
+}
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,23 @@
-fun main(){
+fun main() {
+
+    println(fullName("Asma","Saeed","Almahdawi"))
 
 }
+fun fullName( firstName : String , middleName : String , LastName :String): String{
+
+    return "$firstName $middleName $LastName"
+}
+
+/*
+fun main() {
+    println(math(2,3))
+}
+fun math(num1 : Int , num2 : Int){
+    println( num1 + num2)
+    println( num1 - num2 )
+    println( num1 * num2)
+    println(num1/ num2)
+
+}
+
+ */
