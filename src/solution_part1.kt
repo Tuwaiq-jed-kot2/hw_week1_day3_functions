9a116de6240a6ee37d78f66e5275a0aa7b3874f7
Sirin Alkadi <serenalgadi@gmail.com>
2021-10-13 07:28:53 +0300

    Problem Solved :)

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,13 @@
-fun main(){   }
+fun main(){
+    println(add(10,12))
+    println(fullName("Ahmed","Abdullah","Al Waleed"))
+}
+//Q1
+fun add(num1:Int,num2:Int):Int{
+    return num1 + num2
+}
+
+//Q2
+fun fullName(first:String,middle:String,last:String):String{
+    return("$first $middle $last")
+}
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,36 @@
 fun main(){
+    println(math(2,3))
+    castFireball(7)
+}
+//PartII
 
+//val maximum:(Int) -> String ={
+//    "The largest number is $it "
+//}
+
+//PartIII - Q1
+fun math(num1:Int,num2:Int):String {
+    val add = num1 + num2
+    val sub = num1 - num2
+    val mul = num1 * num2
+    val div = num1 / num2
+    return """
+    |Add: $add
+    |Subtract: $sub
+    |Multiply: $mul
+    |Divide: $div
+    """.trimMargin()
+}
+//Q2
+fun castFireball(fireball:Int) {
+    when(fireball){
+        0 -> println("The remaining fuel is: 50")
+        1 -> println("The remaining fuel is: 43")
+        2 -> println("The remaining fuel is: 36")
+        3 -> println("The remaining fuel is: 29")
+        4 -> println("The remaining fuel is: 22")
+        5 -> println("The remaining fuel is: 15")
+        6 -> println("The remaining fuel is: 8")
+        7 -> println("The remaining fuel is: 1")
+    }
 }
