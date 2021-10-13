176f8ecc3eeed5758e6e36b587b2fa600611630d
Nayef Saeed Alzahrani <NAYEF.SAEED.jed.k2@tuwaiq.edu.sa>
2021-10-13 04:36:28 +0300

    failed to solve part 2 &3

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,14 @@
-fun main(){   }
+fun main()
+//{  //Start The answer:Q1
+// println(add(10,12))
+//}
+//fun add(num1:Int, num2:Int) :Int = num1 + num2 //End The answer:Q1
+
+
+{   //Start The answer:Q2
+    println(fullName("Nayef","Saeed","Al Zahrani"))
+}
+fun fullName (firstName:String, middleName:String, lastName:String ):String = "$firstName $middleName $lastName" //End The answer:Q2
+
+
+
