0c85951d36678193e3f776494e0ddc54bb7592d7
yasminbasim <Yasmin.Basim.jed.k2@tuwaiq.edu.sa>
2021-10-14 12:27:53 +0300

    خطاء في التسليم

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,1 +1,23 @@
-fun main(){   }
+//add two numbers
+/*fun main() {
+    val num1=1000000
+    val num2=12
+    println(addnum(12,10))
+}
+fun addnum(num1:Int,num2:Int):Int {
+    return num1+num2
+}
+
+ */
+//qution 2
+fun main() {
+   fullname()
+}
+fun fullname(){
+    println(names("yasmin","basim","zawger"))
+}
+fun names
+            (name1:String,name2:String,name3:String):String {
+    return " $name1+ $name2 +$name3"
+}
+
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,11 @@
-fun main(){
+//part2
+fun main() {
 
+    fun main() {
+        println(maximum(14, 12, 10))
+        (theResult(15, 9))
+    }
+
+    val maximum = {( num1: Int, num2: Int, num3: Int) ->
+        return(num1,num2,num3) }
 }
