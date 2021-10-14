851ea27fb7518173d2b41fdd0945659905a75981
yasminbasim <Yasmin.Basim.jed.k2@tuwaiq.edu.sa>
2021-10-14 12:36:22 +0300

    خطاء في التسليم

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,11 +1,13 @@
 //part2
-fun main() {
 
     fun main() {
-        println(maximum(14, 12, 10))
-        (theResult(15, 9))
+      maximum()
+    }
+  fun maximum{
+      (14, 12, 10)
+      println(number(15, 9))
+  }
+    val number= {( num1:Int,num2:Int,num3: Int): Int->
+        return(num1,num2,num3)
     }
 
-    val maximum = {( num1: Int, num2: Int, num3: Int) ->
-        return(num1,num2,num3) }
-}
