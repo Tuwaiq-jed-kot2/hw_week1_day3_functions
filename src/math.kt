889f01860501f05d6c608c5e18cf4088e2b406c9
AsrarAlbishri <Asrar.Mohammad.jed.k2@tuwaiq.edu.sa>
2021-10-23 11:49:05 +0300

    solved

--- src/math.kt
+++ src/math.kt
@@ -13,18 +13,22 @@ fun math(){
 }
 
 fun adding (num1: Int , num2: Int):Int{
+    print("add = ")
     return  num1+num2
 
 }
 
 fun sub (num1: Int , num2: Int):Int{
+    print("subtract = ")
     return num1-num2
 }
 
 fun mult (num1: Int , num2: Int):Int{
+    print("multiply = ")
     return num1*num2
 }
 
 fun div (num1: Int , num2: Int):Int{
+    print("divide = ")
     return num1/num2
 }
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,1 +1,16 @@
+fun main() {
 
+    maximum(15, 5 , 8)
+
+}
+
+val maximum : (n1:Int, n2:Int, n3:Int) -> Unit = {  num1, num2, num3 -> Int
+
+    if (num1 >= num2 && num1 >= num3){
+        println("the greatest number is $num1")
+    }else if (num2 >= num1 && num2 >= num3){
+        println("the greatest number is $num2")
+    } else {
+        println("the greatest number is $num3")
+    }
+}
